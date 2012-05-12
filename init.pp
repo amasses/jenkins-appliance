08a78153b441660dcb4bc0d0786e0c61c8a22366
Mick Pollard <aussielunix@gmail.com>
2012-05-12 15:11:17 +1000

    initial appliance checkin

---
+++ init.pp
@@ -0,0 +1,30 @@
+node default {
+  include apt
+  include stdlib
+  include jenkins
+
+
+  $plugins = [ 
+    'git',
+    'chucknorris',
+    'github',
+    'greenballs',
+    'ruby',
+    'rake',
+    'ssh',
+    'monitoring',
+    'favorite',
+    'gravatar',
+    'jabber',
+    'sounds',
+    'radiatorviewplugin',
+    'vsphere-cloud',
+    'warnings',
+    'analysis-core',
+    'rvm',
+    'htmlpublisher',
+    'xfpanel'
+  ]
+
+  jenkins::plugin::install { $plugins: }
+}
